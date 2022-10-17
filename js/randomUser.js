9c97f021e236081ae4c6f4cd4671df1b208baae2
Gabriel García <garciage@outlook.com>
2022-10-17 11:06:58 -0300

    V5.0

---
+++ js/randomUser.js
@@ -0,0 +1,37 @@
+document.getElementById("myBtn").addEventListener("click", getdata);
+
+function getdata() {
+  fetch("https://randomuser.me/api/?results=20")
+    .then((res) => res.json())
+
+    .then((data) => {
+      let user = data.results; //porque así junto los campos para llegar al número que jode
+
+      let output = "";
+
+      user.forEach(function (lists) {
+        output += `
+                
+                <div class="row gy-3">
+                <div class="col-auto">
+                    <div class="card" style="width: 18rem;">
+                        <img src="${lists.picture.medium}" class="card-img-top">
+                        <div class="card-body">
+                          <h5 class="card-title"><strong>${lists.name.first} ${lists.name.last}</strong></p>
+                          <ul class="list-group">
+                          <li class="list-group-item"><p>Dirección: <br>${lists.location.street.name} ${lists.location.street.number}, ${lists.location.city}, ${lists.location.state}, ${lists.location.country}</p></li>
+                            <li class="list-group-item"><p>Email: <br>${lists.email}</p></li>
+                            <li class="list-group-item"><p>Tel.: <br>${lists.cell}</p></li>
+                            <li class="list-group-item"><p>Usuario: <br>${lists.login.username}</p></li>
+                            <li class="list-group-item"><p>Contraseña: <br>${lists.login.password}</p></li>
+                          </ul>    
+                        </div>
+                    </div>
+                </div>
+                </div>
+             `;
+      });
+
+      document.getElementById("output").innerHTML = output;
+    });
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,94 @@
-// function updatemenu() {
-//     if (document.getElementById('responsive-menu').checked == true) {
-//       document.getElementById('menu').style.borderBottomRightRadius = '0';
-//       document.getElementById('menu').style.borderBottomLeftRadius = '0';
-//     }else{
-//       document.getElementById('menu').style.borderRadius = '10px';
-//     }
-//   }
+
+const cabecera = `
+    <header id="header">
+    <section class="top-nav">
+    <div>
+      <img class="logo" src="images/Blue Navy Modern Mobo Tech Logo.gif" alt="logo"></img>
+    </div>
+    <input id="menu-toggle" type="checkbox" />
+    <label class='menu-button-container' for="menu-toggle">
+      <div class='menu-button'></div>
+    </label>
+    <ul class="menu">
+      <li><a href="index.html" target="_blank">Inicio</a></li>
+      <li><a href="nosotros.html" target="_blank">Nosotros</a></li>
+      <li><a href="contacto.html" target="_blank">Contacto</a></li>
+      <li><a href="Threat.html" target="_blank">Mapa de amenazas</a></li>
+
+      <li class='dropdown'>
+        <a class="dropbtn">Saber +</a>
+        <ul class="dropdown-content">
+          <li><a href="index.html#section_1">¿Qué es la ciberseguridad?</a></li>
+          <li><a href="index.html#section_2">Dominios de ciberseguridad</a></li>
+          <li><a href="index.html#section_3">Mitos de la ciberseguridad</a></li>
+          <li><a href="index.html#section_4">Amenazas cibernéticas comunes</a></li>
+          <li><a href="Threat.html" target="_blank">Mapa de amenazas</a></li>
+          <li><a href="index.html#section_5">Tecnologías claves</a></li>
+          <li><a href="index.html#section_6">Estrategias de seguridad</a></li>
+          <li><a href="index.html#section_7">Soluciones</a></li>
+        </ul>
+      </li>
+    </ul>
+  </section>
+  </header>
+  `
+
+  document.getElementById('header').innerHTML = cabecera;
+
+  const footer = `
   
+    <footer class="footer-distributed">
+
+			<div class="footer-left">
+
+            
+
+                  <p class="footer-links">
+                
+                <a href="index.html" target="_blank">Inicio</a>
+					·
+					<a href="nosotros.html" target="_blank">Nosotros</a>
+					·
+					<a href="contacto.html" target="_blank">Contacto</a>
+					·
+					<a href="Threat.html" target="_blank">Mapa de amenazas</a>
+				</p>
+
+				<p class="footer-company-name">Codo a Codo 2022</p>
+
+				<div class="footer-icons">
+
+					<a href="www.facebook.com"><i><img src="https://img.icons8.com/small/32/000000/facebook-new.png"/></i></a>
+					<a href="www.twitter.com"><i><img src="https://img.icons8.com/small/32/000000/twitter.png"/></i></a>
+					<a href="www.instagram.com"><i><img src="https://img.icons8.com/small/32/000000/instagram-new.png"/></i></a>
+					<a href="www.youtube.com"><i><img src="https://img.icons8.com/small/32/000000/youtube-play.png"/></i></a>
+
+				</div>
+
+			</div>
+
+      
+
+      
+
+      
+
+		</footer>
+        `
+
+  document.getElementById('footer').innerHTML = footer;
+
+
+//   <div class="footer-right">
+
+//   <p>Contacto Express</p>
+
+//   <form action="#" method="post">
+
+//     <input type="text" name="email" placeholder="Email">
+//     <textarea name="message" placeholder="Mensaje"></textarea>
+//     <button>Enviar</button>
+
+//   </form>
+
+// </div>
